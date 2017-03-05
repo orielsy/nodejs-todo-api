b80b6ed873b4c24c91ba945c0be7d1be5cf5ed96
Orielsy <orielsy@gmail.com>
2017-03-05 07:20:03 +0000

    add todos

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
+var todos = [{
+        id: 1,
+        description: 'meet mom for lunch',
+        completed: false 
+    },
+    {
+        id: 2,
+        description: 'go to market',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'booty rape',
+        completed: true
+    }
+];
 
 app.get('/', function(req, res){
    res.send('Todo API Root'); 
+});
+
+app.get('/todos', function(req, res){
+   res.json(todos);
+});
+
+//app.get('/todos/')
+app.get('/todos/:id', function(req, res){
+    var todoId = Number(req.params.id);
+    var matchedTodo; 
+    
+    for(var x = 0; x < todos.length; x++){
+        if(todos[x].id == todoId){
+            matchedTodo = todos[x];
+        }
+    }
+    
+    if(matchedTodo == undefined){
+        res.status(404).send();        
+    } else {
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function(){
