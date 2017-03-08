3bbdbf193e9f4e0e7a0ea1f217e86c466b86b6d8
Orielsy <orielsy@gmail.com>
2017-03-08 16:29:11 +0000

    push:

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -6,21 +6,56 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
    description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
    },
    completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
    }
-})
+});
 
-sequelize.sync({force: true}).then(function(){
+sequelize.sync({//force: true
+    }).then(function(){
     console.log('Everything is synced');
     
+    Todo.findById(2).then(function (todo){
+        if (todo){
+            console.log(todo.toJSON());
+        } else {
+            console.log('todo not found');
+        }
+    });
+    
+    /*
     Todo.create({
         description: 'walk dog',
-        completed: true
     }).then(function (todo){
-        console.log('finished');
-        console.log(todo);
-    });
+        return Todo.create({
+            description: 'Clean office'
+        });
+    }).then(function(){
+        //return Todo.findById(1);
+        return Todo.findAll({
+            where: {
+                description: {
+                    $like: '%dog%'
+                }
+            }
+        });
+    }).then(function(todos){
+        if(todos){
+            todos.forEach(function (todo){
+                console.log(todo.toJSON());
+            })
+        } else {
+            console.log('no todo found');
+        }
+    }).catch(function (e){
+        console.log(e);
+    });*/
 });
