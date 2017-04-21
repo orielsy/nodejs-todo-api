4cf8c402c0e99421880af552492da154915a03b2
Orielsy <orielsy@gmail.com>
2017-04-21 05:25:15 +0000

    convert get todo id

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-var db = require('./db.js');
+var db = require('./db.js')
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -35,34 +35,28 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoId = Number(req.params.id);
-    var matchedTodo = _.findWhere(todos, {id: todoId}); 
     
-    if(matchedTodo == undefined){
-        res.status(404).send();        
-    } else {
-        res.json(matchedTodo);
-    }
+    db.todo.findById(todoId).then(function (todo){
+        if(!!todo){
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, function (e){
+        res.status(500).send();
+    });
+    
 });
 
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'completed', 'description');
     
-    db.todo.create(body).then(function (todo){
+    db.todo.create(body).then(function(todo){
        res.json(todo.toJSON()); 
-    }, function (e){
+    }, function(e){
         res.status(400).json(e);
     });
     
-    /*
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
-    
-    body.description = body.description.trim();
-    body.id = todoNextId;
-    todoNextId++;
-    todos.push(body);
-    res.json(body);*/
 });
 
 app.delete('/todos/:id', function(req, res){
@@ -102,9 +96,11 @@ app.put('/todos/:id', function(req, res){
     res.json(matchedTodo);
 });
 
-db.sequelize.sync().then(function(){
+db.sequelize.sync().then(function (){
     app.listen(PORT, function(){
         console.log('express listening on port ' + PORT); 
     });
 });
+
+
 
